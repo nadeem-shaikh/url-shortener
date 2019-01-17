2dbd8eb78be09a9ec87215226cbb86f1fbd6f634
Your Name <you@example.com>
2019-01-17 11:30:28 +1100

    Added Production ENV

--- backend/config/config.js
+++ backend/config/config.js
@@ -15,6 +15,20 @@ var config = {
         "keepAlive": 10000,
         "connectTimeoutMS" : 30000
     }
+  },
+  production :{
+    root: rootPath,
+    baseURL:"http://localhost:3000",
+    app: {
+      name: 'url-shortener'
+    },
+    db: "mongodb+srv://admin:admin@cluster0-x5tfa.mongodb.net/test?retryWrites=true/url-shortener",
+    dbOptions:{
+        "socketTimeoutMS": 240000,
+        "keepAlive": 10000,
+        "connectTimeoutMS" : 30000
+    }
+
   }
 };
 module.exports = config[env];
