c4893f3216d9a0673315e769b7b4c9a0ddc2b7cc
Your Name <you@example.com>
2019-01-17 20:00:13 +1100

    Added Production Variables

--- frontend/src/app/services/api.service.ts
+++ frontend/src/app/services/api.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class ApiService {
-  private API_URL = 'http://localhost:3000/api/v1';
+  private API_HOSTNAME = location.hostname;
+  private API_PORT = environment.port;
+  private API_PROTOCOL = environment.protocol;
+  private API_BASE_URL = this.API_PROTOCOL + '://' + this.API_HOSTNAME + ':' + this.API_PORT + '/api/v1' ;
+  // 'http://localhost:3000/api/v1';
   private URLS_PATH = '/getAllUrls';
   private GENERATE_SHORT_URL_PATH = '/generateShortUrl';
   private ORIGINAL_PATH = '/getOriginalUrl';
@@ -17,20 +22,22 @@ export class ApiService {
   constructor(private http: HttpClient) { }
 
   getLinks() {
-    return this.http.get(this.API_URL + this.URLS_PATH, {
+
+    console.log(this.API_BASE_URL + this.URLS_PATH);
+    return this.http.get(this.API_BASE_URL + this.URLS_PATH, {
       headers: this.HEADERS
     });
   }
 
   shortUrl(url) {
-    return this.http.post(this.API_URL + this.GENERATE_SHORT_URL_PATH , url, {
+    return this.http.post(this.API_HOSTNAME + this.GENERATE_SHORT_URL_PATH , url, {
       headers: this.HEADERS
     });
   }
 
 
   originalUrl(url) {
-    return this.http.get(this.API_URL + this.ORIGINAL_PATH + '/' + url, {
+    return this.http.get(this.API_HOSTNAME + this.ORIGINAL_PATH + '/' + url, {
       headers: this.HEADERS
     });
   }
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  port: 3000,
+  protocol : 'http',
 };
 
 /*
